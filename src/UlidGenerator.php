b4627083c8b5ecd7d751c9973eb205500977e70b
Anton Smirnov <sandfox@sandfox.me>
2023-07-12 01:53:36 +0300

    Use monotonic sequences

--- src/UlidGenerator.php
+++ src/UlidGenerator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Arokettu\Uuid\Doctrine;
 
 use Arokettu\Uuid\UlidFactory;
+use Arokettu\Uuid\UlidMonotonicSequence;
 use Arokettu\Uuid\Uuid;
 
 /**
@@ -12,8 +13,11 @@ use Arokettu\Uuid\Uuid;
  */
 final class UlidGenerator extends AbstractGenerator
 {
+    private static UlidMonotonicSequence $sequence;
+
     public function generateUuid(): Uuid
     {
-        return UlidFactory::ulid();
+        self::$sequence ??= UlidFactory::sequence();
+        return self::$sequence->next();
     }
 }
--- src/UuidV7Generator.php
+++ src/UuidV7Generator.php
@@ -6,14 +6,18 @@ namespace Arokettu\Uuid\Doctrine;
 
 use Arokettu\Uuid\Uuid;
 use Arokettu\Uuid\UuidFactory;
+use Arokettu\Uuid\UuidV7MonotonicSequence;
 
 /**
  * @psalm-api
  */
 final class UuidV7Generator extends AbstractGenerator
 {
+    private static UuidV7MonotonicSequence $sequence;
+
     public function generateUuid(): Uuid
     {
-        return UuidFactory::v7();
+        self::$sequence ??= UuidFactory::v7Sequence();
+        return self::$sequence->next();
     }
 }

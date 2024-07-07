2ed93ea8d93cb29e1e42954a494d32b84f2a1abb
Anton Smirnov <sandfox+git@sandfox.me>
2024-07-08 02:39:53 +0300

    Update code style

--- src/AbstractType.php
+++ src/AbstractType.php
@@ -19,7 +19,7 @@ abstract class AbstractType extends Type
     abstract protected function dbStringToUuid(string $uuid): Uuid;
     abstract protected function externalStringToUuid(string $uuid): Uuid;
 
-    public function convertToPHPValue(mixed $value, AbstractPlatform $platform): ?Uuid
+    public function convertToPHPValue(mixed $value, AbstractPlatform $platform): Uuid|null
     {
         if ($value === null || $value instanceof Uuid) {
             return $value;
@@ -40,7 +40,7 @@ abstract class AbstractType extends Type
         }
     }
 
-    public function convertToDatabaseValue(mixed $value, AbstractPlatform $platform): ?string
+    public function convertToDatabaseValue(mixed $value, AbstractPlatform $platform): string|null
     {
         if ($value === null) {
             return null;
